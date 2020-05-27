831b35ea8ed38f63e82e42b4ed362756f792f319
Reo Shigeno <reoshigeno0708@gmail.com>
2020-05-27 17:56:27 +0900

    Version Sample

---
+++ Pods/Target Support Files/Pods-VersionSampleTests/Pods-VersionSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VersionSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_VersionSampleTests
+@end
--- VersionSample/ViewController.swift
+++ VersionSample/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Storyboard上のLabel等にOutletを繋ぐ
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+
+    let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        versionLabel.text = "\(version) (\(build))"
+        
         // Do any additional setup after loading the view.
     }
 
