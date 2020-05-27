005deb057b38366a778ed38b6a045b9236be5465
Reo Shigeno <reoshigeno0708@gmail.com>
2020-05-27 15:48:32 +0900

    Initial Commit

---
+++ VersionSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VersionSample
+//
+//  Created by 繁野怜央 on 20200527.
+//  Copyright © 2020 Reo.S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
